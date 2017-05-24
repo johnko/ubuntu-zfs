cc6893193141c7f1a2396a1e3d6b5232dc776fc5
John Ko <git@johnko.ca>
2017-05-23 21:29:32 -0400

    upgrade pip

--- skel/bin/_setup-ansible.sh
+++ skel/bin/_setup-ansible.sh
@@ -5,6 +5,6 @@ set -x
 which python2.7 || sudo apt-get install --yes python2.7
 which pip || sudo apt-get install --yes python-pip
 
-sudo pip install pip
+sudo pip install --upgrade pip
 
 sudo pip install ansible
