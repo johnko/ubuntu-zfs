81bccc488001fe820957f60b0d293df8533d17b6
John Ko <git@johnko.ca>
2017-05-23 21:44:59 -0400

    use generic instead of kernel version

--- skel/bin/_setup-virtualbox.sh
+++ skel/bin/_setup-virtualbox.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-sudo apt-get install --yes "linux-headers-$(uname -r)"
+sudo apt-get install --yes linux-headers-generic
 sudo apt-get install --yes virtualbox-5.0
 
 sudo /sbin/rcvboxdrv setup
