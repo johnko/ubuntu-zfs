3bd892328198d47855f6cdb743b7ff085f3aeb73
John Ko <git@johnko.ca>
2017-05-23 21:33:03 -0400

    add dependency (libssl-dev) for ansible

--- skel/bin/_setup-ansible.sh
+++ skel/bin/_setup-ansible.sh
@@ -3,8 +3,11 @@ set -e
 set -x
 
 which python2.7 || sudo apt-get install --yes python2.7
-which pip || sudo apt-get install --yes python-pip
 
+which pip || sudo apt-get install --yes python-pip
 sudo pip install --upgrade pip
+
+# dependency for ansible
+sudo apt-get install --yes libssl-dev
 
 sudo pip install ansible
