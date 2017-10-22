3c3dcd6dcd21ef57803908d389fa0f2427b002da
John Ko <git@johnko.ca>
2017-10-22 18:24:09 -0400

    use apt list

--- skel/bin/_backup-apt-cache.sh
+++ skel/bin/_backup-apt-cache.sh
@@ -8,7 +8,8 @@ mkdir -p "${HOME}/.apt-cache/apt/archives"
 rsync -virtP --exclude lock --exclude partial /var/lib/apt/lists/ "${HOME}/.apt-cache/lists/"
 rsync -virtP --exclude lock --exclude partial /var/cache/apt/ "${HOME}/.apt-cache/apt/"
 cd "${HOME}/.apt-cache/apt/archives"
-dpkg -l | awk '$1~/ii/{print "apt-get download "$2}' | sh -x
+# dpkg -l | awk '$1~/ii/{print "apt-get download "$2}' | sh -x
+apt list --installed 2>/dev/null | awk -F/ '{print "apt-get download "$1}' | sh -x
 cd -
 
 if [ -n "${RSYNC_CACHE_SERVER}" ]; then
