31537d193ff69c75f1776275df921757c6430a34
John Ko <git@johnko.ca>
2017-10-22 23:48:45 -0400

    chmod after append

--- setup-nullmailer.sh
+++ setup-nullmailer.sh
@@ -10,20 +10,20 @@ OUTFILE="/etc/nullmailer/remotes"
 if [ -f "${OUTFILE}" ] && [ ! -f "${OUTFILE}.original" ]; then
   cp -a "${OUTFILE}" "${OUTFILE}.original"
 fi
-chmod 600 "${OUTFILE}"
 if [ -n "${GMAIL_USER}" ] && [ -n "${GMAIL_PASSWORD}" ]; then
   cat >"${OUTFILE}" <<EOF
 smtp.gmail.com smtp --port=587 --starttls --user=${GMAIL_USER} --pass=${GMAIL_PASSWORD}
 EOF
 fi
+chmod 600 "${OUTFILE}"
 
 OUTFILE="/etc/nullmailer/adminaddr"
 if [ -f "${OUTFILE}" ] && [ ! -f "${OUTFILE}.original" ]; then
   cp -a "${OUTFILE}" "${OUTFILE}.original"
 fi
-chmod 644 "${OUTFILE}"
 if [ -n "${GMAIL_USER}" ]; then
   cat >"${OUTFILE}" <<EOF
 ${GMAIL_USER}
 EOF
 fi
+chmod 644 "${OUTFILE}"
