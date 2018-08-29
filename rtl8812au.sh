4eea53b6463f2edcf0118ebbdc6269ac96ae046d
John Ko <git@johnko.ca>
2018-08-28 22:02:58 -0400

    add comments to clarify steps

--- rtl8812au.sh
+++ rtl8812au.sh
@@ -4,6 +4,7 @@ set -x
 
 if lsusb | grep -q -i "ID 2357:010d"; then
 
+  # rtl8812au-dkms is in universe
   which apt-add-repository || apt-get install --yes software-properties-common
   apt-add-repository universe
   if [ -z "${RSYNC_CACHE_SERVER}" ]; then
@@ -21,15 +22,15 @@ if lsusb | grep -q -i "ID 2357:010d"; then
   fi
 
   # rtl8812au-dkms is driver for TP-Link - Archer T4U AC1300, https://github.com/diederikdehaas/rtl8812AU/pull/105/files
+  # only install after linux headers are available
   apt-get install --yes rtl8812au-dkms
 
-  # driver source folder
+  # get driver source folder
   RTL_SRC=$( dpkg -L rtl8812au-dkms | grep -o '^/[^/]*/[^/]*/[^/]*' | uniq -c | sort -nr | head -n 1 | awk '{print $2}' )
 
   # get dynamic module and version
-  #KMOD_NAME="rtl8812au"
+  # example KMOD_NAME="rtl8812au" and KMOD_VER="4.3.8.12175.20140902+dfsg"
   KMOD_NAME=$( echo $RTL_SRC | awk -F/ '{print $NF}' | awk -F- '{print $1}' )
-  #KMOD_VER="4.3.8.12175.20140902+dfsg"
   KMOD_VER=$( echo $RTL_SRC | awk -F/ '{print $NF}' | awk -F- '{print $NF}' )
 
   # patch the usb_intf.c file to add the Archer T4U ID
@@ -46,13 +47,12 @@ VDRVSCAqLwo=
 EOF
   patch -N ${RTL_SRC}/os_dep/linux/usb_intf.c "${PATCH_FILE}" || true
 
-  # this may have already been added during apt install
+  # this may or may not have already been added during install of rtl8812au-dkms
   MP_NAME="8812au"
   # remove the module if exist
   lsmod | grep -q "${MP_NAME}" || modprobe -r ${MP_NAME} || true
-  # uninstall the module
+  # uninstall and remove the module for livecd kernel and new kernel
     dkms uninstall -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_CUR} || true
-  # remove the module source
     dkms remove    -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_CUR} || true
   if [ "${KERN_CUR}" != "${KERN_NEW}" ]; then
     dkms uninstall -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_NEW} || true
@@ -66,9 +66,8 @@ EOF
   #apt-get install --yes libelf-dev
   #cd ${RTL_SRC} && make clean && make && make install
 
-  # add the module source
+  # add, build and install the module source for livecd kernel and new kernel
     dkms add     -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_CUR} || true
-  # rebuild and reinstall the module
     dkms build   -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_CUR}
     dkms install -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_CUR}
   if [ "${KERN_CUR}" != "${KERN_NEW}" ]; then
@@ -76,11 +75,10 @@ EOF
     dkms build   -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_NEW}
     dkms install -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_NEW}
   fi
-  # load module into kernel if not exist
+  # load module into kernel if not exist, then check that kernel module is present
   lsmod | grep -q "${MP_NAME}" || modprobe ${MP_NAME}
-  # check that kernel module is present
   lsmod | grep "${MP_NAME}"
 
-  # check if link exists
+  # check that a network interface exists
   ip link | grep "wlx"
 fi
