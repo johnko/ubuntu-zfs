b18523c95ab9a6c1c60d43617dccced29bfde4b6
John Ko <git@johnko.ca>
2018-08-28 21:59:34 -0400

    build for livecd kernel and new kernel, conditional modprobe

--- rtl8812au.sh
+++ rtl8812au.sh
@@ -3,6 +3,7 @@ set -e
 set -x
 
 if lsusb | grep -q -i "ID 2357:010d"; then
+
   which apt-add-repository || apt-get install --yes software-properties-common
   apt-add-repository universe
   if [ -z "${RSYNC_CACHE_SERVER}" ]; then
@@ -12,6 +13,7 @@ if lsusb | grep -q -i "ID 2357:010d"; then
   # need a compiler and linux headers
   apt-get install --yes build-essential linux-headers-generic
   # current kernel may be different
+  # example livecd has linux-headers-4.15.0-29-generic but debootstrap installs linux-headers-4.15.0-33-generic
   KERN_CUR="$( uname -r )"
   KERN_NEW=$( ls /usr/src/ | grep "linux-headers-.*generic" | tail -n 1 | sed 's;linux-headers-;;' )
   if [ "${KERN_CUR}" != "${KERN_NEW}" ]; then
@@ -45,21 +47,19 @@ EOF
   patch -N ${RTL_SRC}/os_dep/linux/usb_intf.c "${PATCH_FILE}" || true
 
   # this may have already been added during apt install
-  # so remove the module
   MP_NAME="8812au"
-  modprobe -r ${MP_NAME} || true
+  # remove the module if exist
+  lsmod | grep -q "${MP_NAME}" || modprobe -r ${MP_NAME} || true
   # uninstall the module
-  dkms uninstall -m ${KMOD_NAME} -v ${KMOD_VER} || true
+    dkms uninstall -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_CUR} || true
+  # remove the module source
+    dkms remove    -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_CUR} || true
   if [ "${KERN_CUR}" != "${KERN_NEW}" ]; then
     dkms uninstall -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_NEW} || true
+    dkms remove    -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_NEW} || true
   fi
-  # remove the module source
-  dkms remove -m ${KMOD_NAME} -v ${KMOD_VER} -k $(uname -r) || true
-  if [ "${KERN_CUR}" != "${KERN_NEW}" ]; then
-    dkms remove -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_NEW} || true
-  fi
-  # check kernel module is absent
-  lsmod | grep "${MP_NAME}" || true
+  # check kernel module is absent (strings are both empty)
+  test "x" == x$( lsmod | grep "${MP_NAME}" )
 
   # manual build
   # libelf-dev needed to compile driver
@@ -67,25 +67,18 @@ EOF
   #cd ${RTL_SRC} && make clean && make && make install
 
   # add the module source
-  if [ "${KERN_CUR}" != "${KERN_NEW}" ]; then
-    dkms add -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_NEW} || true
-  else
-    dkms add -m ${KMOD_NAME} -v ${KMOD_VER} || true
-  fi
+    dkms add     -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_CUR} || true
   # rebuild and reinstall the module
+    dkms build   -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_CUR}
+    dkms install -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_CUR}
   if [ "${KERN_CUR}" != "${KERN_NEW}" ]; then
-    dkms build -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_NEW}
-  else
-    dkms build -m ${KMOD_NAME} -v ${KMOD_VER}
-  fi
-  if [ "${KERN_CUR}" != "${KERN_NEW}" ]; then
+    dkms add     -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_NEW} || true
+    dkms build   -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_NEW}
     dkms install -m ${KMOD_NAME} -v ${KMOD_VER} -k ${KERN_NEW}
-  else
-    dkms install -m ${KMOD_NAME} -v ${KMOD_VER}
   fi
-  # load module into kernel
-  modprobe ${MP_NAME}
-  # check if kernel module is present
+  # load module into kernel if not exist
+  lsmod | grep -q "${MP_NAME}" || modprobe ${MP_NAME}
+  # check that kernel module is present
   lsmod | grep "${MP_NAME}"
 
   # check if link exists
