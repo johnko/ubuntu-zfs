27a38dae978e936bb86a5d454a737a5c03dd51d1
John Ko <git@johnko.ca>
2018-08-30 13:22:06 -0400

    manipulate /etc/network/interfaces.d only for 16.04; add example how to use nmcli and higher wifi route priority

--- setup-network-interfaces.sh
+++ setup-network-interfaces.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-# Ubuntu 18.04 uses netplan, but for now we want ifupdown
+# Ubuntu 18.04 uses netplan, but for now we still want ifupdown to email us about IP changes
 apt-get install --yes ifupdown
 
 OUTFILE="/etc/network/interfaces"
@@ -16,32 +16,35 @@ cat >"${OUTFILE}" <<EOF
 source ${SOURCEDIR}/*
 EOF
 
-if [ ! -f "${SOURCEDIR}/lo" ]; then
-  cat >"${SOURCEDIR}/lo" <<EOF
+# Only manipulate /etc/network/interfaces.d/ for 16.04
+if lsb_release -r | grep -q "16.04"; then
+  if [ ! -f "${SOURCEDIR}/lo" ]; then
+    cat >"${SOURCEDIR}/lo" <<EOF
 auto lo
 iface lo inet loopback
 EOF
-fi
+  fi
 
-ACTIVE_NICS=$(ip link | grep '^[0-9]' | grep -E -v '(^docker| lo:|NO-CARRIER)' | cut -d' ' -f2 | cut -d: -f1)
-for i in ${ACTIVE_NICS}; do
-  if [ ! -f "${SOURCEDIR}/${i}" ]; then
-    cat >"${SOURCEDIR}/${i}" <<EOF
+  ACTIVE_NICS=$(ip link | grep '^[0-9]' | grep -E -v '(^docker| lo:|NO-CARRIER)' | cut -d' ' -f2 | cut -d: -f1)
+  for i in ${ACTIVE_NICS}; do
+    if [ ! -f "${SOURCEDIR}/${i}" ]; then
+      cat >"${SOURCEDIR}/${i}" <<EOF
 auto ${i}
 iface ${i} inet dhcp
 EOF
-  fi
-done
+    fi
+  done
 
-UNPLUG_NICS=$(ip link | grep '^[0-9]' | grep 'NO-CARRIER' | cut -d' ' -f2 | cut -d: -f1)
-for i in ${UNPLUG_NICS}; do
-  if [ ! -f "${SOURCEDIR}/${i}" ]; then
-    cat >"${SOURCEDIR}/${i}" <<EOF
+  UNPLUG_NICS=$(ip link | grep '^[0-9]' | grep 'NO-CARRIER' | cut -d' ' -f2 | cut -d: -f1)
+  for i in ${UNPLUG_NICS}; do
+    if [ ! -f "${SOURCEDIR}/${i}" ]; then
+      cat >"${SOURCEDIR}/${i}" <<EOF
 #auto ${i}
 #iface ${i} inet dhcp
 EOF
-  fi
-done
+    fi
+  done
+fi
 
 install -d -m 755 -o root -g root /etc/netplan
 cat >>/etc/netplan/01-network-manager-all.yaml <<EOF
@@ -85,3 +88,16 @@ method=auto
 EOF
 chown root: /etc/NetworkManager/system-connections/xxxxxxx
 chmod 600 /etc/NetworkManager/system-connections/xxxxxxx
+
+# How to use nmcli to connect to wifi
+#nmcli dev wifi list
+#nmcli dev wifi con mySSID password myPassword
+#nmcli con down mySSID
+#nmcli con up mySSID
+#nmcli con delete mySSID
+#ip link set down wlx0
+#ip link set up wlx0
+#ip addr
+
+# Temporary give wifi route higher priority
+#ip route add $(ip route | grep 'default.*wlx' | awk '{$NF=50;print}')
