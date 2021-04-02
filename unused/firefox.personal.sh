0ec9a26a5488feb6bc7f8e146f82d2fa49c7121a
johnko <git@johnko.ca>
2021-04-02 19:08:40 -0400

    remove unused

--- unused/firefox.personal.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -x
-
-/Applications/Firefox.app/Contents/MacOS/firefox-bin --profile ~/Library/Application\ Support/Firefox/Profiles/john.personal
--- unused/ubuntu/_setup-virtualbox.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -x
-
-sudo apt-get install --yes linux-headers-generic
-sudo apt-get install --yes virtualbox-5.0
-
-sudo /sbin/rcvboxdrv setup
